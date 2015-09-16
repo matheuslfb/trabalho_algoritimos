c5bb625b71c45b77f74a3f78855a6ab68b9c7ca4
matheuslfb <matheus.ferreira.001@acad.pucrs.br>
2015-09-16 10:01:39 -0300

    Update LTest.java

--- src/LTest.java
+++ src/LTest.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.io.*;
 
@@ -6,12 +7,42 @@ public class LTest {
     public static void main( String[] args ) throws FileNotFoundException {
         List L = new List();
         System.out.println("Lista criada.");
-        Scanner input = new Scanner( new FileReader("casos/x1.txt")).useDelimiter("\n");
+        Scanner input = new Scanner( new FileReader("casos/x1")).useDelimiter("\n");
         
         while ( input.hasNext() ) {
             String temp = input.next();
+            if ( temp.matches( "[0-9]+" ) ) L.insert( Integer.parseInt( temp ) );
+            
+           /*
+            *  if(temp.equals("quit")) System.exit(0);
+            
+            if ( temp.equals( "cont" ) )  System.out.println("Contador = " + L.contador());
+            if ( temp.equals( "existe" ) ) { 
+                int aux = input.nextInt();
+                if ( L.existe( aux ) ) System.out.println("existe o numero");
+                else System.out.println("NAO existe o numero");
+            }
+            if ( temp.equals( "primeiro" ) ) {
+                System.out.println ("o primeiro da lista era " + L.getFirst());
 
-           System.out.println(temp);
+            }
+            if(temp.equals("troca")) L.swap();
+            if ( temp.equals( "append" ) ) { 
+                int aux = input.nextInt();
+                L.append(aux);
+            }
+            if ( temp.equals( "insere" ) )
+                L.insert(input.nextInt(), input.nextInt());
+                
+                
+                L.print();
+            */
+            
+            System.out.println(temp);
+            if ( temp.equals( "cont" ) )  System.out.println("Contador = " + L.contador());
+           
+            
         }
+        
     }
 }
